d5f4d260f0b51aecb63399cd2755e5960afd3e7f
monikamittal-1728 <monikagupta1728@gmail.com>
2026-06-20 01:02:40 +0530

    Add category pills section on home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,19 @@
 import VideoCard from "../components/VideoCard";
 import useFetch from "../hooks/useFetch";
 
+const categories = [
+  "All",
+  "React",
+  "JavaScript",
+  "Node.js",
+  "MongoDB",
+  "Express",
+  "Tailwind",
+  "Web Development",
+  "Programming",
+  "Podcasts",
+];
+
 const HomePage = () => {
   const { data, loading, error } = useFetch(
     "http://localhost:5000/api/videos"
@@ -26,6 +39,33 @@ const HomePage = () => {
 
   return (
     <div className="pt-20 px-4">
+      
+      {/* Category Pills */}
+      <div className="flex gap-3 overflow-x-auto scrollbar-hide mb-6 pb-2">
+        {categories.map((category, index) => (
+          <button
+            key={category}
+            className={`
+              whitespace-nowrap
+              px-3
+              py-1.5
+              rounded-lg
+              text-sm
+              font-medium
+              transition-colors
+              ${
+                index === 0
+                  ? "bg-black text-white"
+                  : "bg-gray-100 hover:bg-gray-200 text-black"
+              }
+            `}
+          >
+            {category}
+          </button>
+        ))}
+      </div>
+
+      {/* Videos */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
         {videoList.map((item) => (
           <VideoCard
